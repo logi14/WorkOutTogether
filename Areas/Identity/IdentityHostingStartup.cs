6155c274e0d9bc6d016edbd074c7e3eea2d0c10c
MrQlek <39280029+MrQlek@users.noreply.github.com>
2019-04-13 12:54:57 +0200

    Register form init

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WorkOutTogether;
+using WorkOutTogether.Data;
+
+[assembly: HostingStartup(typeof(WorkOutTogether.Areas.Identity.IdentityHostingStartup))]
+namespace WorkOutTogether.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
