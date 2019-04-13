8f935aeed084c7c6d5344451dd330310d1248840
Piotr Sidor <piotr.sid14@gmail.com>
2019-04-13 10:19:54 +0200

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorkOutTogether.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
